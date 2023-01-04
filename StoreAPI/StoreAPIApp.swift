16e54a6e7f6aa6c51c9fad1be3c822c9a8d6f895
Rumen Guin <rumen2k@gmail.com>
2023-01-04 23:54:58 +0530

    Initial Commit

---
+++ StoreAPI/StoreAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoreAPIApp.swift
+//  StoreAPI
+//
+//  Created by RUMEN GUIN on 04/01/23.
+//
+
+import SwiftUI
+
+@main
+struct StoreAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
